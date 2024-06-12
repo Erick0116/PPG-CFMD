e9d670baf00ba25b617fa76c4526125461f32f93
jannerick11689@gmail.com <jannerick11689@gmail.com>
2024-06-12 16:27:08 +0000

    Edited Main.VehicleList, settings and Search

--- client_code/Main/VehicleList/__init__.py
+++ client_code/Main/VehicleList/__init__.py
@@ -16,3 +16,21 @@ class VehicleList(VehicleListTemplate):
 
   def btn_search_click(self, **event_args):
     self.rp_vehicles.items  = anvil.server.call('search_vehicles', self.txt_search.text)
+
+  def btn_details_click(self, **event_args):
+    search_plateno  = self.txt_search.text
+    vehicle_info = anvil.server.call('search_vehicles', search_plateno)
+    print("Results:", vehicle_info)
+
+"""
+    if vehicle_info is not None and len(vehicle_info) >= 4:
+      make = vehicle_info[0]
+      body_type = vehicle_info[1]
+      color = vehicle_info[2]
+      plate_no = vehicle_info[3]
+        # Display the make in an alert
+      alert(f"Make: {make}, Body Type: {body_type}, Color: {color}, PlateNo: {plate_no}")
+    else:
+      alert('Plate number doesn\'t exist!')
+  
+"""
--- server_code/Search.py
+++ server_code/Search.py
@@ -13,39 +13,12 @@ def search_vehicles(query):
   return results
 
 @anvil.server.callable
-def view_details(query):
+def view_details(search_plateno):
   results = app_tables.vehicles.search()
-  if query:
-    filtered_results  = [
-      {
-        'make': x['make'],
-        'body_type':  x['body_type'],
-        'color': x['color'],
-        'model':  x['model'],
-        'register_name': x['register_name'],
-        'plate_no': x['plate_no'],
-        'engine_no':  x['chasis_no'],
-        'area_assigned':  x['area_assigned'],
-        'status':  x['status'],
-        'cr_no': x['cr_no'],
-        'date_cr': x['date_cr'],
-        'or_no':  x['or_no'],
-        'date_renewed_or': x['date_renewed_or'],
-        'next_renewal': x['next_renewal'],
-        'insurance_type': x['insurance_type'],
-        'insurance_name': x['insurance_name'],
-        'premium': x['premium'],
-        'coverage': x['coverage'],
-        'expiry': x['expiry'],
-        'remarks': x['remarks']
-      }
-      for x in results
-      if query in x['plate_no']
-    ]
-    if filtered_results:
-      return filtered_results[0]
-    else:
-      return None
-  else:
-    return None
-  
+  return results
+ # print("Search Results:", results)
+  #if results and len(results) > 0:
+   # first_result = results[0]
+   # return [first_result['make'], first_result['body_type'], first_result['color'], first_result['plate_no']]
+  #else:
+   # return None
