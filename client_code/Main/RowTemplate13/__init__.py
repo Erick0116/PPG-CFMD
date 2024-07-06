7c1ab533799777f16756d9591afba66b5801adb4
jannerick11689@gmail.com <jannerick11689@gmail.com>
2024-07-06 16:05:40 +0000

    Created Main.RowTemplate13.RowTemplate16; Edited Main, Main.RowTemplate15 and Main.RowTemplate13

--- client_code/Main/RowTemplate13/__init__.py
+++ client_code/Main/RowTemplate13/__init__.py
@@ -12,6 +12,25 @@ class RowTemplate13(RowTemplate13Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.data_row_panel_write_view.visible = False
+    self.expanded  = False
+    self.expanded2 = False
+    self.expanded_panel_1.visible = False
+    self.expanded_panel_remarks.visible = False
+  def toggle_expand(self, **event_args):
+    if not self.expanded:
+      self.expanded_panel_1.visible = True
+      self.expanded = True
+    else:
+      self.expanded_panel_1.visible = False
+      self.expanded = False
+
+  def toggle_expand2(self, **event_args):
+    if not self.expanded2:
+      self.expanded_panel_remarks.visible = True
+      self.expanded2 = True
+    else:
+      self.expanded_panel_remarks.visible = False
+      self.expanded2 = False
 
     # Any code you write here will run before the form opens.
 
@@ -23,9 +42,21 @@ class RowTemplate13(RowTemplate13Template):
     self.data_row_panel_write_view.visible = False
     self.data_row_panel_read_view.visible = True
 
-  def btn_approve_click(self, **event_args):
+  def btn_save_click(self, **event_args):
     record = anvil.server.call('update_repTicketStatus', self.lbl_requestno.text, self.dp_status.selected_value)
     if record:
       alert(f"Record {self.lbl_plateno.text} status hass been updated  successfully!")
+      open_form('Main') 
     else:
       alert("Update failed")
+
+  def btn_view_click(self, **event_args):
+    self.toggle_expand()
+
+  def btn_approval_click(self, **event_args):
+    self.toggle_expand2()
+
+  def dp_status_change(self, **event_args):
+    if self.dp_status.selected_value == "Approved":
+      self.toggle_expand2()
+    
--- client_code/Main/RowTemplate15/__init__.py
+++ client_code/Main/RowTemplate15/__init__.py
@@ -18,3 +18,11 @@ class RowTemplate15(RowTemplate15Template):
   def btn_edit_onhold_click(self, **event_args):
     self.data_row_panel_write_view_pending.visible = True
     self.data_row_panel_read_view_pending.visible = False
+
+  def btn_updateOnhold_click(self, **event_args):
+    record = anvil.server.call('update_repTicketStatus', self.lbl_requestnoOnhold.text, self.dp_status_onhold.selected_value)
+    if record:
+      alert(f"Record {self.lbl_requestnoOnhold.text} status hass been updated  successfully!")
+      open_form('Main')
+    else:
+      alert("Update failed")
